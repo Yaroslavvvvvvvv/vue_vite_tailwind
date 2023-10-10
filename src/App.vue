f7ad81377c9446f43f8b320f8e8ffe00601cdf3a
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-10 13:30:32 +0300

    add random footer, first section from tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import MyHeader from "./components/MyHeader.vue";
+import MyFooter from "./components/MyFooter.vue";
+import FirstSection from "./components/FirstSection.vue";
 </script>
 
 <template>
   <MyHeader/>
+  <FirstSection/>
+  <MyFooter/>
 </template>
 
 <style scoped>
