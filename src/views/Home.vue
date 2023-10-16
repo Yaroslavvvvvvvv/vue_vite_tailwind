c5dcef98470e23b760e7899ffaf2f00e71948ee0
pavloruban1 <137701866+pavloruban1@users.noreply.github.com>
2023-10-16 19:03:35 +0300

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,13 @@
 
 
 import AboutContent from "../components/AboutContent.vue";
+import ProductList from "../components/ProductList.vue";
 </script>
 
 <template>
   <AboutContent/>
+  <ProductList/>
+
 </template>
 
 <style scoped>
