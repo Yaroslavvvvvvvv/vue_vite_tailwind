580d8d456d43d185b157b1f442c49e45b965d4c9
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-17 02:05:15 +0300

    add catalog in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,12 @@
 
 
 import AboutContent from "../components/AboutContent.vue";
+import MyCatalog from "./MyCatalog.vue";
 </script>
 
 <template>
   <AboutContent/>
+  <MyCatalog/>
 </template>
 
 <style scoped>
